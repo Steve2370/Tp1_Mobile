6556a550c0d398ae330d2a2eb27ba9968a887efa
Steve2370 <briceyouatchui@gmail.com>
2024-09-20 10:25:48 -0400

    Initial Commit

---
+++ Tp1_Mobile/Tp1_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tp1_MobileApp.swift
+//  Tp1_Mobile
+//
+//  Created by Leon Beltran on 2024-09-20.
+//
+
+import SwiftUI
+
+@main
+struct Tp1_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tp1_MobileTests/Tp1_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tp1_MobileTests.swift
+//  Tp1_MobileTests
+//
+//  Created by Leon Beltran on 2024-09-20.
+//
+
+import Testing
+@testable import Tp1_Mobile
+
+struct Tp1_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
